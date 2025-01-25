c95df2abab5f33b771dfe1b285641f2039ad07ea
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-01-25 20:33:15 -0300

    Created User and Role entities.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@email.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@email.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@email.com', '123456');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
