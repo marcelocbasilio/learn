8781d9ab2e0697b2d44ae6c77a86c8ece060b4aa
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-01-26 14:54:55 -0300

    Implemented entity Resource.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,3 +19,7 @@ INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'pa
 
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-01-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2026-01-25T20:00:00.12345Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2026-12-25T20:00:00.12345Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'path_img.png', 1, 'link externo', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'path_img.png', 2, 'link externo', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Lives', 'Lives exclusivas para turma', 3, 'path_img.png', 0, 'link externo', 1);
