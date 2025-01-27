a0daae920b0f2b91f9811f836aa5a93ccff560f5
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-01-26 21:44:26 -0300

    Implemented entities Reply and Topic.

--- src/main/java/com/marcelocbasilio/learn/entities/Topic.java
+++ src/main/java/com/marcelocbasilio/learn/entities/Topic.java
@@ -4,41 +4,38 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_topic")
 public class Topic implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @ManyToMany
+    @JoinTable(name = "tb_topic_likes", joinColumns = @JoinColumn(name = "topic_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private final Set<User> likes = new HashSet<>();
+    @OneToMany(mappedBy = "topic")
+    private final List<Reply> replies = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
     @Column(columnDefinition = "TEXT")
     private String body;
-
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-
     @ManyToOne
     @JoinColumn(name = "author_id")
     private User author;
-
     @ManyToOne
     @JoinColumn(name = "offer_id")
     private Offer offer;
-
     @ManyToOne
     @JoinColumn(name = "lesson_id")
     private Lesson lesson;
-
-    @ManyToMany
-    @JoinTable(name = "tb_topics_likes", joinColumns = @JoinColumn(name = "topic_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private Set<User> likes = new HashSet<>();
+    @ManyToOne
+    @JoinColumn(name = "reply_id")
+    private Reply answer;
 
     public Topic() {
     }
@@ -51,5 +48,106 @@ public class Topic implements Serializable {
         this.author = author;
         this.offer = offer;
         this.lesson = lesson;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public Reply getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Reply answer) {
+        this.answer = answer;
+    }
+
+    public Set<User> getLikes() {
+        return likes;
+    }
+
+    public List<Reply> getReplies() {
+        return replies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Topic topic = (Topic) o;
+        return Objects.equals(id, topic.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", body='" + body + '\'' +
+               ", moment=" + moment +
+               ", author=" + author +
+               ", offer=" + offer +
+               ", lesson=" + lesson +
+               ", answer=" + answer +
+               ", likes=" + likes +
+               ", replies=" + replies +
+               '}';
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,56 +1,112 @@
-INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
-INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
-INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority)
+VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority)
+VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority)
+VALUES ('ROLE_ADMIN');
 
-INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@email.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@email.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@email.com', '123456');
+INSERT INTO tb_user (name, email, password)
+VALUES ('Alex Brown', 'alex@email.com', '123456');
+INSERT INTO tb_user (name, email, password)
+VALUES ('Bob Brown', 'bob@email.com', '123456');
+INSERT INTO tb_user (name, email, password)
+VALUES ('Maria Green', 'maria@email.com', '123456');
 
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (3, 3);
 
-INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Texto', TIMESTAMP WITH TIME ZONE '2025-01-25T20:00:00.12345Z', false, 'Rota', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id)
+VALUES ('Texto', TIMESTAMP WITH TIME ZONE '2025-01-25T20:00:00.12345Z', false, 'Rota', 1);
 
-INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'path_img.png', 'path_img_gray.png');
+INSERT INTO tb_course (name, img_uri, img_gray_uri)
+VALUES ('Bootcamp HTML', 'path_img.png', 'path_img_gray.png');
 
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-01-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2026-01-25T20:00:00.12345Z', 1);
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2026-12-25T20:00:00.12345Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id)
+VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-01-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE
+        '2026-01-25T20:00:00.12345Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id)
+VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-25T20:00:00.12345Z', TIMESTAMP WITH TIME ZONE
+        '2026-12-25T20:00:00.12345Z', 1);
 
-INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'path_img.png', 1, 'link externo', 1);
-INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'path_img.png', 2, 'link externo', 1);
-INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Lives', 'Lives exclusivas para turma', 3, 'path_img.png', 0, 'link externo', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id)
+VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'path_img.png', 1, 'link externo', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id)
+VALUES ('Fórum', 'Tire suas dúvidas', 2, 'path_img.png', 2, 'link externo', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id)
+VALUES ('Lives', 'Lives exclusivas para turma', 3, 'path_img.png', 0, 'link externo', 1);
 
-INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'path_img.png', 1, null);
-INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos começar', 2, 'path_img.png', 1, 1);
-INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos começar', 3, 'path_img.png', 1, 2);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
+VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'path_img.png', 1, null);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
+VALUES ('Capítulo 2', 'Neste capítulo vamos começar', 2, 'path_img.png', 1, 1);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id)
+VALUES ('Capítulo 3', 'Neste capítulo vamos começar', 3, 'path_img.png', 1, 2);
 
-INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', null, true, false);
-INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update)
+VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update)
+VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', null, true, false);
 
-INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do Capítulo 1', 1, 1);
-INSERT INTO tb_content (id, text_content, video_uri) VALUES (1, 'Material de Apoio: abc', 'link de um video');
+INSERT INTO tb_lesson (title, position, section_id)
+VALUES ('Aula 1 do Capítulo 1', 1, 1);
+INSERT INTO tb_content (id, text_content, video_uri)
+VALUES (1, 'Material de Apoio: abc', 'link de um video');
 
-INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 do Capítulo 1', 2, 1);
-INSERT INTO tb_content (id, text_content, video_uri) VALUES (2, '', 'link de um video');
+INSERT INTO tb_lesson (title, position, section_id)
+VALUES ('Aula 2 do Capítulo 1', 2, 1);
+INSERT INTO tb_content (id, text_content, video_uri)
+VALUES (2, '', 'link de um video');
 
-INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 do Capítulo 1', 3, 1);
-INSERT INTO tb_content (id, text_content, video_uri) VALUES (3, '', 'link de um video');
+INSERT INTO tb_lesson (title, position, section_id)
+VALUES ('Aula 3 do Capítulo 1', 3, 1);
+INSERT INTO tb_content (id, text_content, video_uri)
+VALUES (3, '', 'link de um video');
 
-INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do Capítulo 1', 4, 1);
-INSERT INTO tb_task (id, description, question_count, approval_count, weight, due_date) VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2025-01-26T13:00:00.12345Z');
+INSERT INTO tb_lesson (title, position, section_id)
+VALUES ('Tarefa do Capítulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_count, approval_count, weight, due_date)
+VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2025-01-26T13:00:00.12345Z');
 
-INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
-INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id)
+VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id)
+VALUES (2, 1, 1);
 
-INSERT INTO tb_deliver (uri, moment, status, feedback, correct_count, lesson_id, user_id, offer_id) VALUES ('', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 0, null, null, 4, 1, 1);
+INSERT INTO tb_deliver (uri, moment, status, feedback, correct_count, lesson_id, user_id, offer_id)
+VALUES ('', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 0, null, null, 4, 1, 1);
 
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 1', 'Corpo do tópico 1', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 1);
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 2', 'Corpo do tópico 2', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 1);
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 3', 'Corpo do tópico 3', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 1);
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 4', 'Corpo do tópico 4', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 2);
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 5', 'Corpo do tópico 5', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 2);
-INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id) VALUES ('Título do tópico 6', 'Corpo do tópico 6', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 3);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 1', 'Corpo do tópico 1', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 2', 'Corpo do tópico 2', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 3', 'Corpo do tópico 3', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 1);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 4', 'Corpo do tópico 4', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 2);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 5', 'Corpo do tópico 5', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1, 2);
+INSERT INTO tb_topic (title, body, moment, author_id, offer_id, lesson_id)
+VALUES ('Título do tópico 6', 'Corpo do tópico 6', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 2, 1, 3);
+
+INSERT INTO tb_topic_likes (topic_id, user_id)
+VALUES (1, 2);
+INSERT INTO tb_topic_likes (topic_id, user_id)
+VALUES (2, 1);
+
+INSERT INTO tb_reply (body, moment, topic_id, author_id)
+VALUES ('Tentei reiniciar o computador', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 2);
+INSERT INTO tb_reply (body, moment, topic_id, author_id)
+VALUES ('Deu certo, valeu!', TIMESTAMP WITH TIME ZONE '2025-01-25T13:00:00.12345Z', 1, 1);
+
+INSERT INTO tb_reply_likes (reply_id, user_id)
+VALUES (1, 1);
